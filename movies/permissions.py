2d6d2df19e4a04b9cc99e526a8631f204167a57f
Kibet Ronoh <kibethillaire@gmail.com>
2021-10-25 23:26:18 +0300

    Updated permissions

--- movies/permissions.py
+++ movies/permissions.py
@@ -15,3 +15,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the creator of the movie
         return obj.creator == request.user
+
+def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the creator of the movie
+        return obj.creator == request.user
