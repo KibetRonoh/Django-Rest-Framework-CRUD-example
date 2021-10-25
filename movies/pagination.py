06ab46c57e0187b8e785eb5a333b8ac80156c594
Kibet Ronoh <kibethillaire@gmail.com>
2021-10-25 23:34:56 +0300

    Update Pagination

--- movies/pagination.py
+++ movies/pagination.py
@@ -4,3 +4,7 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
