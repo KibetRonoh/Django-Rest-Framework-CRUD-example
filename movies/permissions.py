9a405f294f1f0344814adba987be32d4712a0851
Kibet Ronoh <kibethillaire@gmail.com>
2021-11-23 20:33:19 +0300

    update

--- movies/permissions.py
+++ movies/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
 from rest_framework.exceptions import PermissionDenied
-from rest_framework.exceptions import PermissionDenied
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
